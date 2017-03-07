9f6242a60a86211cad7ba0ca92aad035174eabe8
VampDude <stefan.neotokyo@gmail.com>
2017-03-07 18:58:59 +0100

    Create Vehicles.cs

---
+++ Vehicles.cs
@@ -0,0 +1,25 @@
+using System;
+namespace CarAndTrucks_Homework
+{
+	public abstract class Vehicles
+	{
+		public string LicencePlate { get; set; }
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int Year { get; set; }
+		public string Color { get; set; }
+		public double Power { get; set; }
+
+
+		public virtual void PrintInfo()
+		{
+			Console.WriteLine($@"
+Licence plates: {LicencePlate},
+Make: {Make},
+Model: {Model},
+Year: {Year},
+Color: {Color},
+Power: {Power} KW");
+		}
+	}
+}
